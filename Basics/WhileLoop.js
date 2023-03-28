0b6c73086bef1518acfeefdfbcd91542e846ab39
tusharanand7893 <tushar.anand7893@gmail.com>
2023-03-29 00:15:23 +0530

    While and do while loop

---
+++ Basics/WhileLoop.js
@@ -0,0 +1,19 @@
+let i=1;
+
+while(i<10){
+   //code
+    console.log("loop running "+i);
+    i++;
+}
+
+let j=1;
+
+do{
+    console.log ("do loop first"+j);
+    j++;
+    //do this until while condition fails
+}
+while(j<10){
+    console.log ("while loop "+j);
+   
+}
