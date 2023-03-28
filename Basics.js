dada46cb435c07dc683a1e4773d4c1926b91eebf
tusharanand7893 <tushar.anand7893@gmail.com>
2023-03-28 23:47:34 +0530

    print and variables

---
+++ Basics.js
@@ -0,0 +1,14 @@
+console.log("hello world Tushar") // to print
+
+//variables
+
+//var, let and const
+
+let a=8;
+
+
+let s="String";
+
+let bollean=true;
+
+console.log("number= "+a+" String = "+s+" bollean= "+bollean);
