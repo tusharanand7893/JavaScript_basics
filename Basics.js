7b6db22c6de140415bdb37aebfe6797bd5ac0ebd
tusharanand7893 <tushar.anand7893@gmail.com>
2023-03-28 23:57:49 +0530

    variables doc

--- Basics.js
+++ Basics.js
@@ -12,3 +12,17 @@ let s="String";
 let bollean=true;
 
 console.log("number= "+a+" String = "+s+" bollean= "+bollean);
+
+//redeclare only with var
+var r="String";
+var r=10;
+
+//reassign with var and let
+let l="String";
+//let l=10; not possible with let
+l=10;
+
+//const does not allow redeclare and reassign
+const c=10;
+//const c=1;
+//c=2;
