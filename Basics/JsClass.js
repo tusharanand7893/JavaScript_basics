6f0beb816ed44f00c1057febea6edab929cf8b2a
tusharanand7893 <tushar.anand7893@gmail.com>
2023-03-29 15:52:04 +0530

    Creating classes in JS

---
+++ Basics/JsClass.js
@@ -0,0 +1,35 @@
+//creating class with constructor and variables
+
+class Person{
+
+    
+    get age(){
+        return 25;
+    }
+
+    get location(){
+        return "Pune";
+    }
+
+   get fullname(){
+    return this.firstName+" "+this.lastName;
+   }
+
+    constructor(firstName,lastName){
+        this.firstName= firstName; //instanse variables and properties
+        this.lastName=lastName;
+    }
+
+    //methods
+    fullnamefunction(){
+        console.log(this.firstName+" "+this.lastName);
+    }
+
+}
+
+//creating object of class and accessing value and functions
+let person= new Person("Tushar","Anand");
+console.log(person.fullname);
+console.log(person.location);
+console.log(person.age);
+console.log(person.fullnamefunction());
